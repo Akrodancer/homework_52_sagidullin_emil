b4e77387597c72d5d1558ed4770906b17d471001
SagidullinEmil <se12373@auca.kg>
2023-11-28 15:29:22 +0600

    Создана миграция

---
+++ to_do_list/to_do_list_app/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class ToDoList (models.Model):
+    description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='Описание')
+    status = models.TextField(max_length=20, null=False, blank=False, default='New', verbose_name='Статус')
+    date = models.DateField(default='', null=False, verbose_name='Дата')
+
+    def __str__(self):
+        return f'{self.pk}. {self.description}. {self.status}. {self.date}'
