403f7138b2aedec8a381abd44088fde6f6415491
SagidullinEmil <se12373@auca.kg>
2023-12-08 13:00:41 +0600

    обновление миграций и url

--- to_do_list/to_do_list_app/urls.py
+++ to_do_list/to_do_list_app/urls.py
@@ -6,6 +6,8 @@ from to_do_list_app import views as to_do_list_app_views
 urlpatterns = [
     path('', to_do_list_app_views.render_index, name='index'),
     path('tasks/create', to_do_list_app_views.create, name='create'),
-    path('task/<int:pk>', to_do_list_app_views.render_detailed, name='detailed')
+    path('task/<int:pk>', to_do_list_app_views.render_detailed, name='detailed'),
+    path('task/<int:pk>/update/', to_do_list_app_views.update, name='update'),
+    path('task/<int:pk>/delete/', to_do_list_app_views.delete_view, name='task_delete')
 ]
 
